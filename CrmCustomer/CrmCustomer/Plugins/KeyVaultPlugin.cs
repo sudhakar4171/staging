1af6e458b5eb687a5f55e4df371fdba0b3edb677
Sudhakar Reddy D V <Sudhakar.Reddy@microsoft.com>
2016-09-08 13:36:14 -0700

    updated plugin

--- CrmCustomer/CrmCustomer/Plugins/KeyVaultPlugin.cs
+++ CrmCustomer/CrmCustomer/Plugins/KeyVaultPlugin.cs
@@ -31,22 +31,6 @@ namespace KeyVaultPlugin
 
             AssertNull(organizationService, "organizationService");
 
-            // Obtain the target entity from the input parmameters.
-            Entity accountEntity = (Entity)context.InputParameters["Target"];
-            
-            //create new task for account set in 2 weeks in the future
-            Entity contactAccountTask = new Entity("task");
-            contactAccountTask["subject"] = "Check new account is happy";
-            contactAccountTask["description"] = "Make contact with new customer. See if they are happy with service and resolve any issues.";
-            contactAccountTask["scheduledstart"] = DateTime.Now.AddDays(14);
-            contactAccountTask["scheduledend"] = DateTime.Now.AddDays(14);
-            EntityReference entRef = new EntityReference("account", accountEntity.Id);
-            contactAccountTask["regardingobjectid"] = entRef;
-
-            // Create the task and this should be linked to the new account record
-            organizationService.Create(contactAccountTask);
-
-
             // just create some dummy files and save on disk
             string logsFolder = Environment.ExpandEnvironmentVariables(@"%HOME%\LogFiles");
             Guid randomFileName = Guid.NewGuid();
